554b56b8de1e6d0daee5a89af6f6b2802f8ea71c
Vaishnav <vshnav51@gmail.com>
2019-02-04 15:04:18 +0530

    Add project files.

---
+++ SearchFilter/Controllers/SkillsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SearchFilter.Models;
+using PagedList;
+
+namespace SearchFilter.Controllers
+{
+    public class SkillsController : Controller
+    {
+        private SkillSearchEntities3 db = new SkillSearchEntities3();
+
+        // GET: Skills
+        public ActionResult Index(string sortOrder, string searchString, string SelectSkill)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.SkillName = SelectSkill;
+
+            var employees = from s in db.Skills select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                employees = employees.Where(s => s.SkillName.Contains(searchString));
+            }
+            if (!(SelectSkill == null))
+            {
+                /*var Skilllist = SelectSkill.ToList();
+                employees = employees.Where(s => s.SkillName.Contains(Skilllist.ToString()));*/
+                employees = from s in db.Skills where s.SkillName.Equals(SelectSkill) select s;
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    employees = employees.OrderBy(s => s.SkillName);
+                    break;
+                default:
+                    employees = employees.OrderBy(s => s.SkillCode);
+                    break;
+            }
+            return View(employees.ToList());
+        }
+
+        // GET: Skills/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Skill skill = db.Skills.Find(id);
+            if (skill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(skill);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Details(Skill skill)
+        {
+            string code = skill.SkillCode;
+
+            return View();
+        }
+
+        // GET: Skills/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Skills/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SkillCode,SkillName")] Skill skill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Skills.Add(skill);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(skill);
+        }
+
+        // GET: Skills/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Skill skill = db.Skills.Find(id);
+            if (skill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(skill);
+        }
+
+        // POST: Skills/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SkillCode,SkillName")] Skill skill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(skill).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(skill);
+        }
+
+        // GET: Skills/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Skill skill = db.Skills.Find(id);
+            if (skill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(skill);
+        }
+
+        // POST: Skills/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Skill skill = db.Skills.Find(id);
+            db.Skills.Remove(skill);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SearchFilter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SearchFilter.Startup))]
+namespace SearchFilter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
